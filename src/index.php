20095f2f3f5ed76c7cdf011ac17fc104eb074d5e
Sami Dahoux <sami@bendou.space>
2025-12-31 19:03:18 +0100

    landing hero

---
+++ src/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <meta charset="utf-8" />
+  <title>
+    Sami Dahoux - Bendou
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+  <header>
+    <h1><a href="/">sami.bendou.space</a></h1>
+
+    <nav>
+      <ul>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <div style="position: relative;">
+    <main>
+      <section id="hero">
+        <div class="marquee">
+          <div role="figure">Hi my name is Sami Dahoux!&nbsp;</div>
+          <div aria-hidden="true" role="figure">Hi my name is Sami Dahoux!&nbsp;</div>
+        </div>
+
+        <div class="marquee">
+          <div role="figure">I make software using passion and magic!&nbsp;</div>
+          <div aria-hidden="true" role="figure">I make software using passion and magic!&nbsp;</div>
+        </div>
+        <ul>
+          <li><a class="cta" href="#about">Discover</a></li>
+          <li><a class="cta" href="/assets/DAHOUX-Sami-generic-resume.pdf">Get resume</a></li>
+        </ul>
+      </section>
+    </main>
+  </div>
+
+  <footer>
+    <p id="copyright">
+      © <a href="https://portfolio.bendou.space">Sami Dahoux</a> and
+      <a href="https://www.clarapeker.com/">Clara Peker</a> 2017-2025, All Rights Reserved
+    </p>
+  </footer>
+</body>
